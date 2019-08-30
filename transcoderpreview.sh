4fc69b8e6ce56bd76d67e216220f7f51106fe5ae
deva5610 <40216384+deva5610@users.noreply.github.com>
2019-08-31 01:57:16 +0800

    Update transcoderpreview.sh

--- transcoderpreview.sh
+++ transcoderpreview.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-mv plex-common.sh plex-common.sh.bak
-curl -O https://raw.githubusercontent.com/deva5610/pms-docker/master/root/plex-common.sh
-chmod +x plex-common.sh
-curl -J -L -o /tmp/plexmediaserver.deb "https://artifacts.plex.tv/plex-media-server-alpha/1.16.7.1574-4215f2c66/debian/plexmediaserver_1.16.7.1574-4215f2c66_amd64.deb"
+mv version.txt version.txt.bak
+curl -O https://github.com/deva5610/pms-docker/raw/master/version.txt
+curl -J -L -o /tmp/plexmediaserver.deb "https://artifacts.plex.tv/plex-media-server-alpha/1.16.7.1597-a6e223f7f/debian/plexmediaserver_1.16.7.1597-a6e223f7f_amd64.deb"
 dpkg -i --force-confold /tmp/plexmediaserver.deb
 rm -f /tmp/plexmediaserver.deb
