70c6649afd002266220da52f868f4775fba750c3
deva5610 <40216384+deva5610@users.noreply.github.com>
2019-09-03 13:47:30 +0800

    Update transcoderpreview.sh

--- transcoderpreview.sh
+++ transcoderpreview.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-mv version.txt version.txt.bak
-curl -O https://raw.githubusercontent.com/deva5610/pms-docker/master/version.txt
 curl -J -L -o /tmp/plexmediaserver.deb "https://artifacts.plex.tv/plex-media-server-alpha/1.16.7.1597-a6e223f7f/debian/plexmediaserver_1.16.7.1597-a6e223f7f_amd64.deb"
 dpkg -i --force-confold /tmp/plexmediaserver.deb
 rm -f /tmp/plexmediaserver.deb
+echo "1.16.7.1597-a6e223f7f" > version.txt
